7b8486e729ebb9fdc9c572c4d20f30f04593b251
John Kosmicke <kozjob@yahoo.com>
2021-03-21 00:58:00 -0700

    first cut sorts

---
+++ simple_sort_swap_sort.py
@@ -0,0 +1,31 @@
+
+
+def minimum(arr):
+    min_index, min_value = 0, arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] < min_value:
+            min_index, min_value = i, arr[i]
+    return min_value, min_index
+    
+def simple_sort(arr):
+    ret_arr, working_arr = [], arr.copy()
+    while len(working_arr) > 0:
+        min_value, min_index = minimum(working_arr)
+        ret_arr.append(min_value)
+        working_arr = working_arr[:min_index] + working_arr[min_index +1:]
+    return ret_arr
+
+def swap_sort(arr):
+    num_swaps = 1
+    while num_swaps > 0:
+        num_swaps = 0
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                num_swaps += 1
+    return arr
+
+numbers = [5,8,2,2,4,3,0,2,-5,3.14,2]
+results_arr = [-5, 0, 2, 2, 2, 2, 3, 3.14, 4, 5, 8]
+assert(simple_sort(numbers) == results_arr)
+assert(swap_sort(numbers) == results_arr)
