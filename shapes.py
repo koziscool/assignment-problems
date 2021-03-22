3e96f74aec39f7f6a06b97c05cf5127539551dcd
John Kosmicke <kozjob@yahoo.com>
2021-03-21 19:45:50 -0700

    refactored with abstract base class Shape

--- shapes.py
+++ shapes.py
@@ -3,17 +3,13 @@ import math
 import matplotlib.pyplot as plt
 plt.style.use('bmh')
 
-
-class Rectangle:
+class Shape:
     def __init__(self, base, height, color):
         self.base = base
         self.height = height
         self.color = color
 
-        self.perimeter = 2 * self.base + 2 * self.height
-        self.area = self.base * self.height
-        self.vertices = [ (0,0), (self.base, 0), (self.base, self.height), (0, self.height) ]
-        self.plot_filename = "rectangle.png"
+        self.plot_filename = "shape.png"
 
     def describe(self):
         print("Base: ", self.base)
@@ -36,37 +32,25 @@ class Rectangle:
         plt.savefig(self.plot_filename)
         plt.clf()
 
-
-class RightTriangle:
+class Rectangle(Shape):
     def __init__(self, base, height, color):
-        self.base = base
-        self.height = height
-        self.color = color
+        super().__init__(base, height, color)
+
+        self.perimeter = 2 * self.base + 2 * self.height
+        self.area = self.base * self.height
+        self.vertices = [ (0,0), (self.base, 0), (self.base, self.height), (0, self.height) ]
+        self.plot_filename = "rectangle.png"
+
+
+class RightTriangle(Shape):
+    def __init__(self, base, height, color):
+        super().__init__(base, height, color)
 
         self.perimeter = self.base +  self.height + math.sqrt(self.base**2 + self.height**2)
         self.area = self.base * self.height / 2
         self.vertices = [ (0,0), (self.base, 0), (0, self.height) ]
+        self.plot_filename = "triangle.png"
 
-    def describe(self):
-        print("Base: ", self.base)
-        print("Height: ", self.height)
-        print("Color: ", self.color)
-        print("Perimeter: ", self.perimeter)
-        print("Area: ", self.area)
-        print("Vertices: ", self.vertices)
-        print()
-
-    def render(self):
-        x_values = [ i for i,j in self.vertices ] + [ self.vertices[0][0] ]
-        y_values = [ j for i,j in self.vertices ] + [ self.vertices[0][1] ] 
-        plt.plot(
-            x_values, 
-            y_values,
-            color=self.color
-        )
-        plt.gca().set_aspect("equal")
-        plt.savefig('triangle.png')
-        plt.clf()
 
 class Square(Rectangle):
     def __init__(self, side, color ):
